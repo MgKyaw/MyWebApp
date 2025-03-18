75df77efd806942b7a3e6e0e4d5b5afbd81c9da1
MgKyaw <mgkyaw20112@gmail.com>
2025-03-18 21:44:29 +0700

    feat: Add custom middleware 1

--- Program.cs
+++ Program.cs
@@ -8,4 +8,10 @@ app.UseRewriter(new RewriteOptions().AddRedirect("history", "about"));
 app.MapGet("/", () => "Welcome to Contoso!");
 app.MapGet("/about", () => "Contoso was founded in 2000.");
 
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"{context.Request.Method} {context.Request.Path} {context.Response.StatusCode}");
+    await next(); 
+});
+
 app.Run();
