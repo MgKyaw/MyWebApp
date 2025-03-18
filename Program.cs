9dc6027a142cab255b93cd93508993844fdc28f0
MgKyaw <mgkyaw20112@gmail.com>
2025-03-18 21:26:01 +0700

    feat: Modify response data

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello .NET Developer Community!");
+app.MapGet("/", () => "Welcome to Contoso!");
 
 app.Run();
