62934cea124fe5ff1fc976ae2d09fa05e3845ee6
MgKyaw <mgkyaw20112@gmail.com>
2025-03-18 21:49:22 +0700

    feat: Change the order of middlewares

--- Program.cs
+++ Program.cs
@@ -3,15 +3,15 @@ using Microsoft.AspNetCore.Rewrite;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.UseRewriter(new RewriteOptions().AddRedirect("history", "about"));
-
-app.MapGet("/", () => "Welcome to Contoso!");
-app.MapGet("/about", () => "Contoso was founded in 2000.");
-
 app.Use(async (context, next) =>
 {
     Console.WriteLine($"{context.Request.Method} {context.Request.Path} {context.Response.StatusCode}");
     await next(); 
 });
+
+app.UseRewriter(new RewriteOptions().AddRedirect("history", "about"));
+
+app.MapGet("/", () => "Welcome to Contoso!");
+app.MapGet("/about", () => "Contoso was founded in 2000.");
 
 app.Run();
