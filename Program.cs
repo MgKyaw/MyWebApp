97861fc637ef1f1eda54eda0f8e59554b8bdfa8b
MgKyaw <mgkyaw20112@gmail.com>
2025-03-18 21:28:14 +0700

    feat: Use UseRewrite middleware

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Rewrite;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+app.UseRewriter(new RewriteOptions().AddRedirect("history", "about"));
 
 app.MapGet("/", () => "Welcome to Contoso!");
 app.MapGet("/about", () => "Contoso was founded in 2000.");
